872bf40a4df62c68c8bd395981621b1dde38a25f
Felipe Jung <felipemattoseu@gmail.com>
2021-03-25 11:48:42 -0400

    Add stripe checkout and subscription process

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "services/stripe";
+import { getSession } from "next-auth/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        {
+          price: "price_1IXq7RAj29YgoVdLuTK8MJ8h",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).send("Method not allowed");
+  }
+};
