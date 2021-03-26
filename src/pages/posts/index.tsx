5d9f969424adb14996a92464f34c9860d0b5d3c7
Felipe Jung <felipemattoseu@gmail.com>
2021-03-26 11:36:27 -0400

    Fetch posts from prismic in getStaticProps

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
 import Head from "next/head";
+
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+import { getPrismicClient } from "services/prismic";
+
 import styles from "./styles.module.scss";
 
-const Posts = () => (
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostsProps = {
+  posts: Post[];
+};
+
+const Posts = ({ posts }: PostsProps) => (
   <>
     <Head>
       <title>Posts | Ignews</title>
@@ -9,24 +26,50 @@ const Posts = () => (
 
     <main className={styles.container}>
       <div className={styles.posts}>
-        <a href="">
-          <time>asdasdasd</time>
-          <strong>asdadasdasd</strong>
-          <p>asefasef</p>
-        </a>
-        <a href="">
-          <time>asdasdasd</time>
-          <strong>asdadasdasd</strong>
-          <p>asefasef</p>
-        </a>
-        <a href="">
-          <time>asdasdasd</time>
-          <strong>asdadasdasd</strong>
-          <p>asefasef</p>
-        </a>
+        {posts.map((post) => (
+          <a key={post.slug} href="#">
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
+          </a>
+        ))}
       </div>
     </main>
   </>
 );
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt:
+      post.data.content.find((content) => content.type === "paragraph")?.text ??
+      "",
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
 
 export default Posts;
