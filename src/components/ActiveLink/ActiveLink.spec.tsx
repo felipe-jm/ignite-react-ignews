655d6fd40158165960b6de35382f242634b96666
Felipe Jung <felipemattoseu@gmail.com>
2021-05-25 09:37:11 -0400

    Test ActiveLink component

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,14 +1,34 @@
 import { render } from "@testing-library/react";
 import { ActiveLink } from ".";
 
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
 describe("<ActiveLink>", () => {
   it("should render correctly", () => {
-    const { debug } = render(
+    const { getByText } = render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink>
     );
 
-    debug();
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("should receive an active class when active", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toHaveClass("active");
   });
 });
