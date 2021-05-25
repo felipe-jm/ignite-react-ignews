8e7fad881f954fe68acab160d3b89aec120f9d24
Felipe Jung <felipemattoseu@gmail.com>
2021-05-25 09:36:55 -0400

    Fix tests setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+  modulePaths: ["<rootDir>/src/", "<rootDir>/.jest"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
   },
 };
